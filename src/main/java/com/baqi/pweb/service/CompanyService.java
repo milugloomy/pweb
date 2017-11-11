58a68e462ee5207eb881ef809cf00bb318e2313f
milugloomy <milugloomy@163.com>
2017-11-11 12:54:32 +0800

    poi

--- src/main/java/com/baqi/pweb/service/CompanyService.java
+++ src/main/java/com/baqi/pweb/service/CompanyService.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.poi.xwpf.usermodel.LineSpacingRule;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
@@ -14,6 +15,7 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.baqi.pweb.bean.Company;
 import com.baqi.pweb.common.BaqiException;
@@ -63,7 +65,7 @@ public class CompanyService{
 		XWPFDocument document = null;
 		FileOutputStream os = null;
 		
-		String fileName=sdf.format(new Date())+".docx";
+		String fileName="公司详情_"+sdf.format(new Date())+".docx";
 		try{
 			document = new XWPFDocument();
 			os = new FileOutputStream(new File(docPath+fileName));
@@ -80,24 +82,33 @@ public class CompanyService{
 	        	XWPFParagraph para0 = document.createParagraph();  
 	    		XWPFRun run0 = para0.createRun();
 	    		run0.setColor("FF0000");
-	    		run0.setText(i+"、");
+	    		run0.setText((i+1)+"：");
 	    		
-	    		XWPFParagraph para1 = document.createParagraph();  
-	    		XWPFRun run1 = para1.createRun();
-	    		run1.setBold(true); //加粗 
-	    		run1.setFontSize(14);
-	    		run1.setText("公司名：");
-	    		run1 = para1.createRun();  
-	    		run1.setFontSize(14);
-	    		run1.setText("我的公司");
-	    		XWPFParagraph para2 = document.createParagraph();  
-	    		XWPFRun run2 = para2.createRun();
-	    		run2.setBold(true); //加粗  
-	    		run2.setFontSize(14);
-	    		run2.setText("电    话：");
-	    		run2 = para2.createRun();  
-	    		run2.setFontSize(14);
-	    		run2.setText("13807191145");
+	        	Company company=list.get(i);
+	    		if(!StringUtils.isEmpty(company.getName())){
+		    		para(document,"公司名",company.getName());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getTaxno())){
+	    			para(document,"税    号",company.getTaxno());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getAddress())){
+	    			para(document,"地    址",company.getAddress());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getTel())){
+	    			para(document,"电    话",company.getTel());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getBank())){
+	    			para(document,"开户行",company.getBank());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getBankno())){
+	    			para(document,"行    号",company.getBankno());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getCardno())){
+	    			para(document,"账    号",company.getCardno());
+	    		}
+	    		if(!StringUtils.isEmpty(company.getOwner())){
+	    			para(document,"法    人",company.getOwner());
+	    		}
 	    		
 	    		//换行  
 	            XWPFRun runBlank = document.createParagraph().createRun();  
@@ -117,4 +128,16 @@ public class CompanyService{
 		return fileName;
 	}
 	
+	private void para(XWPFDocument document,String title,String value){
+		XWPFParagraph para1 = document.createParagraph();
+		//行高
+		para1.setSpacingBetween(18, LineSpacingRule.EXACT);
+		XWPFRun run1 = para1.createRun();
+		run1.setBold(true); //加粗 
+		run1.setFontSize(14);
+		run1.setText(title+"：");
+		run1 = para1.createRun();  
+		run1.setFontSize(14);
+		run1.setText(value);
+	}
 }
